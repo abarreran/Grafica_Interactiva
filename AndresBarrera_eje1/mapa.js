5825a01d5f5444aa4ef46390d94a854d35209784
Andres Barrera <abarreran@unal.edu.co>
2024-12-16 17:16:29 -0500

    Primer commit

---
+++ AndresBarrera_eje1/mapa.js
@@ -0,0 +1,59 @@
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el primer punto.
+function Punto1() {
+	document.getElementById("Info").innerHTML = '<h1>Geranium ☆☆☆</h1><br><img class="mapafoto" src="Imagenes/Gerannium.jpg" alt="punto uno"><br><br><p>Geranium en Copenhague tiene tres estrellas en la Guía Michelin Nordic Cities 2022, fue clasificado como el mejor restaurante del mundo en 2022 y está dirigido por el mejor chef del mundo en 2011, Rasmus Kofoed.<br>Dirección: Per Henrik Lings Allé 4, 8., 2100 København Ø</p><a class="enlaceresaltado" href="http://www.geranium.dk/en/" target="_blank">http://www.geranium.dk/en/</a>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el segundo punto.
+function Punto2() {
+	document.getElementById("Info").innerHTML = '<h1>Noma ☆☆☆</h1><br><img class="mapafoto" src="Imagenes/noma.jpg" alt="punto dos"><br><br><p>El restaurante que convirtió a Dinamarca en una nación gastronómica.<br>Dirección: Refshalevej 96, 1432 København K</p> <a class="enlaceresaltado" href="http://www.noma.dk/" target="_blank">http://www.noma.dk/</a>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el tercer punto.
+function Punto3() {
+	document.getElementById("Info").innerHTML = '<h1>Kong Hans Kælder ☆☆</h1><br><img class="mapafoto" src="Imagenes/konghk.jpg" alt="punto tres"><br><br><p>Kong Hans Kælder fue el primer restaurante de Copenhague en recibir una estrella Michelin en 1983 y desde entonces ha mantenido sus altísimos estándares.<br>Dirección: Vingårdstræde 6, 1070 København K</p><a class="enlaceresaltado" href="http://www.konghans.dk/" target="_blank">hhttp://www.konghans.dk/</a>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el cuarto punto.
+function Punto4() {
+	document.getElementById("Info").innerHTML = '<h1>Alchemist ☆☆</h1><br><img class="mapafoto" src="Imagenes/alche.png" alt="punto cuatro"><br><br><p>¡Espera lo inesperado! Ese es el mantra del restaurante Alchemist, galardonado con 2 estrellas Michelin. Detrás de sus enormes puertas de bronce se esconde un teatro, 50 platos y una experiencia gastronómica holística que te dejará boquiabierto.<br>Dirección: Refshalevej 173C, 1432 Copenhague K</p><a class="enlaceresaltado" href="http://alchemist.dk/" target="_blank">http://alchemist.dk/</a>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el quinto punto.
+function Punto5() {
+	document.getElementById("Info").innerHTML = '<h1>Restaurant AOC ☆☆</h1><br><img class="mapafoto" src="Imagenes/aoc.jpg" alt="punto cinco"><br><br><p>AOC posee dos estrellas en la Guía Michelin de Ciudades Nórdicas y está ubicado en el centro de Copenhague .<br>Dirección: Dronningens Tværgade 2, 1302 København K</p><a class="enlaceresaltado" href="http://www.restaurantaoc.dk/" target="_blank">http://www.restaurantaoc.dk/';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el sexto punto.
+function Punto6() {
+	document.getElementById("Info").innerHTML = '<h1>Restaurante Jordnær ☆☆</h1><br><img class="mapafoto" src="Imagenes/jordnr.jpg" alt="punto seis"><br><br><p>Restaurante Jordnær: comida con los pies en la tierra y 2 estrellas Michelin a sólo 10 kilómetros al norte de Copenhague.<br>Dirección: Gentoftegade 29, 2820 Gentofte</p><a class="enlaceresaltado" href="http://www.restaurantjordnaer.dk/" target="_blank">http://www.restaurantjordnaer.dk/';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el septimo punto.
+function Punto7() {
+	document.getElementById("Info").innerHTML = '<h1>Koan ☆☆</h1><br><img class="mapafoto" src="Imagenes/koan.jpg" alt="punto siete"><br><br><p>El restaurante Koan, de dos estrellas Michelin, de inspiración coreana, ha abierto sus puertas en el paseo marítimo de Copenhague en Langeliniekaj. Creado por el ex chef noma Kristian Baumann, también propietario del restaurante coreano Juju en Østerbro.<br>Dirección: Langeliniekaj 5, 2100 Copenhague Ø</p><a class="enlaceresaltado" href="http://www.koancph.dk/" target="_blank">http://www.koancph.dk/';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el octavo punto.
+function Punto8() {
+	document.getElementById("Info").innerHTML = '<h1>Formel B ☆</h1><br><img class="mapafoto" src="Imagenes/dormel.jpg" alt="punto ocho"><br><br><p>Disfrute de una experiencia gastronómica con estrella Michelin en el corazón de Frederiksberg en Formel B, un restaurante moderno donde el gusto, la calidad y la buena mesa van de la mano.<br>Dirección: Vesterbrogade 182, 1800 Frederiksberg C</p><a class="enlaceresaltado" href="https://formelfamily.dk/formelb/" target="_blank">https://formelfamily.dk/formelb/';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el noveno punto.
+function Punto9() {
+	document.getElementById("Info").innerHTML = '<h1>Aloutte ☆</h1><br><img class="mapafoto" src="Imagenes/aloutte.jpg" alt="punto nueve"><br><br><p>El que quizás sea el restaurante más bonito de la ciudad es también el más difícil de encontrar. Alouette, decorado con estrellas Michelin, se esconde en un barrio rústico de Islands Brygge y sirve sabores contemporáneos y refinados con los mejores productos de la temporada.<br>Dirección: Sturlasgade 14P, 1., 2300 København S</p><a class="enlaceresaltado" href="http://www.restaurant-alouette.dk/" target="_blank">http://www.restaurant-alouette.dk/</a>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el decimo punto.
+function Punto10() {
+	document.getElementById("Info").innerHTML = '<h1>JATAK ☆</h1><br><img class="mapafoto" src="Imagenes/jatak.jpg" alt="punto diez"><br><br><p>JATAK, que abrió sus puertas en enero de 2022, es uno de los restaurantes flamantes de Copenhague, y ahora también con estrella Michelin.<br>Dirección: Rantzausgade 39, 2200 København N</p><a class="enlaceresaltado" href="https://jatak.superbexperience.com/" target="_blank">https://jatak.superbexperience.com/';
+	document.getElementById("Info").scrollIntoView();
+}
